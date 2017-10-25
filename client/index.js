402f0824074a7b96cdfc2d5e40dad4e564ca4559
madison bullard <madisonbullard@Madisons-Mac-Pro.local>
2017-10-25 18:10:10 -0400

    adding NODE_ENV stuff

--- client/index.js
+++ client/index.js
@@ -1,5 +1,7 @@
+import dotenv from 'dotenv';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+dotenv.config()
 ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -10,60 +10,68 @@ const paths = {
   JS: path.resolve(__dirname, './client'),
 };
 
-module.exports = {
-  entry: path.join(paths.JS, 'index.js'),
-  output: {
-    path: paths.DIST,
-    filename: 'index_bundle.js'
-  },
-  devServer: {
-    contentBase: paths.SRC,
-    inline: true,
-    open: true,
-    port: 3000
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new MinifyPlugin(),
-    new HtmlWebpackPlugin({
-      template: path.join(paths.SRC, 'index.html'),
-    }),
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, './client/img/ico'),
-        to: path.resolve(__dirname, './dist/images/ico'),
-      },
-    ]),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader',
-        ],
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          'file-loader?limit=1024&name=images/[name].[ext]',
-        ],
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
-        use: [
-          'file-loader?limit=1024&name=fonts/[name].[ext]',
-        ],
-      },
+module.exports = (env = {}) => {
+  // Use env.<YOUR VARIABLE> here:
+  console.log('NODE_ENV: ', env.NODE_ENV) // 'local'
+  console.log('Production: ', env.production) // true
+  return {
+    entry: path.join(paths.JS, 'index.js'),
+    output: {
+      path: paths.DIST,
+      filename: 'index_bundle.js'
+    },
+    node: {
+      fs: 'empty'
+    },
+    devServer: {
+      contentBase: paths.SRC,
+      inline: true,
+      open: true,
+      port: 3000
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new MinifyPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.join(paths.SRC, 'index.html'),
+      }),
+      new CopyWebpackPlugin([
+        {
+          from: path.resolve(__dirname, './client/img/ico'),
+          to: path.resolve(__dirname, './dist/images/ico'),
+        },
+      ]),
     ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: [
+            'babel-loader',
+          ],
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          use: [
+            'file-loader?limit=1024&name=images/[name].[ext]',
+          ],
+        },
+        {
+          test: /\.(eot|svg|ttf|woff|woff2)$/,
+          use: [
+            'file-loader?limit=1024&name=fonts/[name].[ext]',
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+  }
 }
 
